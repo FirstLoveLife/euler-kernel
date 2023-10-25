ef1c6790d737ab79356fdbda647ed275f38f424e
Jie Liu <liujie375@h-partners.com>
2023-10-25 16:48:23 +0800

    Revert "sched: Add per_cpu cluster domain info and cpus_share_lowest_cache API"
    kunpeng inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8E8NN
    CVE: NA
    
    ----------------------------------------------------------------------
    
    This reverts commit 53ad6bf76d9c646e3c8494ed82d90f304c50de1f.
    Signed-off-by: Jie Liu <liujie375@h-partners.com>
    Signed-off-by: Slim6882 <yangjunshuo@huawei.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3014,19 +3014,7 @@ bool cpus_share_cache(int this_cpu, int that_cpu)
 	return per_cpu(sd_llc_id, this_cpu) == per_cpu(sd_llc_id, that_cpu);
 }
 
-/*
- * Whether CPUs are share lowest cache, which means LLC on non-cluster
- * machines and LLC tag or L2 on machines with clusters.
- */
-bool cpus_share_lowest_cache(int this_cpu, int that_cpu)
-{
-	if (this_cpu == that_cpu)
-		return true;
-
-	return per_cpu(sd_lowest_cache_id, this_cpu) == per_cpu(sd_lowest_cache_id, that_cpu);
-}
-
-static inline bool ttwu_queue_cond(int cpu, int wake_flags)
+static inline bool ttwu_queue_cond(int cpu)
 {
 	/*
 	 * If the CPU does not share cache, then queue the task on the
